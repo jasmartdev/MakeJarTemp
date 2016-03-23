aeefd7ca747a9bbc599edc2db70a8049e4f4fac4
jasmartdev <jasmartdevregistry@gmail.com>
2016-03-23 20:37:41 +0700

    + Add all source for template

---
+++ config.bat
@@ -0,0 +1,14 @@
+echo *** config var and path ***
+
+set PRJ_DIR=%~dp0
+
+set JAVA_HOME=C:\DevTools\jdk1.6.0
+set PRJ_NAME=JSDLibs
+set VERSION=100
+
+set PROJ_DIR=%~dp0.
+set SRC_PATH=%PROJ_DIR%\src
+set COMP_PATH=%PROJ_DIR%\compile
+set RELEASE_PATH=%PROJ_DIR%\release
+set LIBS_PATH=%PROJ_DIR%\libs
+set CLASS_PATH=%LIBS_PATH%\android.jar
---
+++ make.bat
@@ -0,0 +1,25 @@
+@echo off
+cls
+
+echo ******* wiki make app *******
+
+echo ******* call config.bat *******
+call config.bat
+
+
+if exist %COMP_PATH% rmdir %COMP_PATH% /s /q
+if exist %RELEASE_PATH% rmdir %RELEASE_PATH% /s /q
+if exist tmp rmdir tmp /s /q
+md %COMP_PATH%
+md %RELEASE_PATH%
+md tmp
+
+copy /y %SRC_PATH%\*.* tmp\
+echo *************** make class ***************
+"%JAVA_HOME%\bin\javac.exe" -classpath %CLASS_PATH% tmp\*.java -d %COMP_PATH%
+rmdir tmp /s /q
+
+echo *************** make jar ***************
+"%JAVA_HOME%\bin\jar.exe" cfM "%PRJ_NAME%_%VERSION%.jar" -C "%COMP_PATH%" .
+copy %PRJ_NAME%_%VERSION%.jar %RELEASE_PATH%\
+del %PRJ_NAME%_%VERSION%.jar
